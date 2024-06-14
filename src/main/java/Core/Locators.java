89cc0d585edda5b172d0b3b728b8ca79b9b6b8f9
NarcisKG <dusan224@gmail.com>
2024-06-14 18:34:21 +0200

    Commit for Git

--- src/main/java/Core/Locators.java
+++ src/main/java/Core/Locators.java
@@ -1,4 +1,18 @@
 package Core;
 
+import org.openqa.selenium.By;
+
 public class Locators {
+
+public static By pretragaProizvoda = By.xpath("//button[@class='megabtn ']//span[text()='Proizvodi']");
+public static By setPretragaProizvoda(String vrstaProizvoda){
+    String proizvodLocator = "//button[@class='megabtn ']//span[text()='*****']";
+    return By.xpath(proizvodLocator.replace("*****",vrstaProizvoda));}
+
+public static By pretragaLaptopKategorije = By.xpath("//a[@href='/prenosni-racunari'][text()='Laptop računari']");
+public static By odabirLaptopKategorije = By.xpath("//a[@href='/prenosni-racunari/laptop-racunari'][text()='Laptop računari']");
+
+
+
+
 }
