b529e3291e5661c0086c5fc5f1921f3a92423f28
NarcisKG <dusan224@gmail.com>
2024-06-17 18:04:48 +0200

    Commit for Git

--- src/main/java/Core/Locators.java
+++ src/main/java/Core/Locators.java
@@ -12,6 +12,25 @@ public static By setPretragaProizvoda(String vrstaProizvoda){
 public static By pretragaLaptopKategorije = By.xpath("//a[@href='/prenosni-racunari'][text()='Laptop računari']");
 public static By odabirLaptopKategorije = By.xpath("//a[@href='/prenosni-racunari/laptop-racunari'][text()='Laptop računari']");
 
+public static By setFilterValue(String filterValue){
+    String filterValueLocator = "(//div[@class='filter']//span[text()='*****'])[1]";
+    return By.xpath(filterValueLocator.replace("*****", filterValue));}
+
+
+
+public static By filterTipProcesora = By.xpath("//span[@id='collapsible-trigger-1718632811832']");
+public static By odabirTipProcesora = By.xpath();
+
+public static By filterRamMemorije = By.xpath();
+public static By setFilterTipRamDdrVga (String filterRamDdrVga){
+    String ramDDRVgaLokator = "//span[@id='collapsible-trigger-1718632811833'][text()='*****']";
+    return By.xpath(ramDDRVgaLokator.replace("*****",filterRamDdrVga));}
+
+public static By setSortOrder(String sortOrder){
+
+return this;}
+public static By searchResult = By.xpath();
+
 
 
 
